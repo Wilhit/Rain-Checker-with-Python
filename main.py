07582cdc164b47dda442c5ce113aa7e57239150f
Wilhit <41920891+Wilhit@users.noreply.github.com>
2021-12-08 21:56:54 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
+LON = 17.0832
+LAT = -22.5594
+
+# https://api.openweathermap.org/
+# Get the api_key, account_sid, auth_token from the website above
+
+api_key = "580bdc9a6f3e73e779fe5ffb0d073935"
+
+account_sid = "AC58ad97c673dae1b3eaef582f5e700c05"
+
+auth_token = "23246e442eac128ad805bfffdc297a70"
+
+parameters = {
+    'lat': LAT,
+    'lon': LON,
+    'appid': api_key,
+    'exclude': 'current,minutely,daily'
+}
+
+response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+# weather_data["hourly"]
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+for hour_data in weather_slice:
+    condition_code = int(hour_data["weather"][0]['id'])
+    if condition_code < 700:
+        will_rain =True
+
+
+if will_rain:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages \
+        .create(
+        body="\n\nIt's going to rain today. Remember to bring an Umbrella, and remember to smile.\n\nI love you, Asvia",
+        from_='+18507717404',
+        to='+264816345342',
+    )
+    print(message.sid)
+
+else:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages \
+        .create(
+        body="\n\nNo chances of raining today. Have a good day and remember to always smile.\n\nI love you, Asvia",
+        from_='+18507717404',
+        to='+264812702607',
+    )
+    print(message.sid)
+
+# print(weather_slice)
+# print(weather_data["hourly"][0]['weather'][0])
+# print(weather_data)
+
